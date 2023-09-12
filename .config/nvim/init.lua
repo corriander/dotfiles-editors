3857c026ee90aea4ca546b118242f92d1d5a51b4
Alex Corrie <ajccode@gmail.com>
2023-09-12 10:24:18 +0100

    feat: merge remaps & options into new init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -74,26 +74,15 @@ require('lazy').setup(lazyconf, {})
 -- Set highlight on search
 vim.o.hlsearch = false
 
--- Make line numbers default
-vim.wo.number = true
-
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
 -- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
+-- vim.o.breakindent = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+-- vim.o.ignorecase = true
+-- vim.o.smartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
@@ -105,8 +94,7 @@ vim.o.timeoutlen = 300
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+require("corriander.set")
 
 -- [[ Basic Keymaps ]]
 
@@ -118,7 +106,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- [[ Highlight on yank ]]
+require("corriander.remap")
+
+--- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
--- .config/nvim/lua/corriander/remap.lua
+++ .config/nvim/lua/corriander/remap.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = " "
-
 -- Project commands
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
