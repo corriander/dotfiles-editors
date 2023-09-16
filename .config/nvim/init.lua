ae3029554b9c20d6b6e822b62850d87e29d7bd5d
Alex Corrie <ajccode@gmail.com>
2023-09-16 12:39:13 +0100

    refactor: move options out of init.lua (and tweak)
    The options section is now in my imported custom set.lua
    
    Some options have been enabled when moving them out because they have
    proved to be good choices.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -67,32 +67,8 @@ vim.opt.rtp:prepend(lazypath)
 local lazyconf = require("corriander.lazy")
 require('lazy').setup(lazyconf, {})
 
+
 -- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
-
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Enable break indent
--- vim.o.breakindent = true
-
--- Case-insensitive searching UNLESS \C or capital in search
--- vim.o.ignorecase = true
--- vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
 
 require("corriander.set")
 
--- .config/nvim/lua/corriander/set.lua
+++ .config/nvim/lua/corriander/set.lua
@@ -1,19 +1,40 @@
+-- NOTE: use of vim.{o,wo} is due to merging kickstart.nvim config here
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- Use hybrid relative line-numbering
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Configure indentation behaviour
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.o.breakindent = true
 
 vim.opt.smartindent = true
 
+-- Configure undo/swap
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undo"
 vim.opt.undofile = true
 
-vim.opt.termguicolors = true
-
+-- Theme / visual configuration
+vim.o.hlsearch = false
+vim.wo.signcolumn = 'yes'
 vim.opt.colorcolumn = "80"
+vim.opt.termguicolors = true
 
