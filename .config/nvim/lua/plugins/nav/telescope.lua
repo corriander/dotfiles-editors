c00db2726a4b984254622e635b11d370d5b9b009
Alex Corrie <ajccode@gmail.com>
2023-09-20 11:12:11 +0100

    refactor: merge original telescope customisations

--- .config/nvim/lua/plugins/nav/telescope.lua
+++ .config/nvim/lua/plugins/nav/telescope.lua
@@ -57,6 +57,15 @@ return {
         vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
         vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]resume' })
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', function()
+        	builtin.find_files({ hidden = true })
+        end)
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', function()
+        	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        end)
     end
   },
 
