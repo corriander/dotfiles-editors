374dfd7eded3556305a83a8caac17a99eaf2804e
Alex Corrie <ajccode@gmail.com>
2023-09-26 01:28:45 +0100

    feat: add fancy notifications
    Helps inform what's going on with new custom lsp / post-install config
    happening async

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
+    {
+        -- Used for some of my custom configuration for fancy notifications
+        'rcarriga/nvim-notify',
+        opts = { background_colour = '#000000' },
+        config = true,
+    },
     { import = "corriander.lazy" },
     { import = "plugins.nav" },
 }
