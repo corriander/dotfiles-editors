c0cf9cd7d64341322f31f403566f38f7f7e0118e
Alex Corrie <ajccode@gmail.com>
2023-10-13 12:43:12 +0100

    refactor: move tabnine and black specs
    They both belong in "lsp" (this also removes the after/ config for
    tabnine in favour of lazy opts)

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -99,4 +99,24 @@ return {
             'rafamadriz/friendly-snippets',
         },
     },
+
+    {
+        'codota/tabnine-nvim',
+        build = "./dl_binaries.sh",
+        opts = {
+              disable_auto_comment=true,
+              accept_keymap="<Tab>",
+              dismiss_keymap = "<C-]>",
+              debounce_ms = 800,
+              suggestion_color = {gui = "#808080", cterm = 244},
+              exclude_filetypes = {"TelescopePrompt", "neo-tree", "oil"},
+              log_file_path = vim.fn.stdpath('cache') .. "/tabnine.log", -- absolute path to Tabnine log file
+        },
+        config = function(_, opts)
+            require('tabnine').setup(opts)
+        end,
+    },
+
+    'psf/black',
+
 }
