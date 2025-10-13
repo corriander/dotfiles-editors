13d051e94ed48f533df6546ea7a82fd185dab351
Alex Corrie <ajccode@gmail.com>
2025-10-13 12:41:04 +0100

    build(deps): add todo-comment plugin

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -90,4 +90,13 @@ return {
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
 
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+    }
 }
