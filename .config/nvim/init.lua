da98d2097e0234273e4fbce22e2ef11f12bc7adb
Alex Corrie <ajccode@gmail.com>
2026-02-05 11:41:13 +0000

    hack: get mjml filetype syntax detection working (quick n dirty)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,6 +168,13 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+-- Configure MJML filetype to use HTML syntax
+vim.filetype.add({
+  extension = {
+    mjml = "html",
+  },
+})
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
